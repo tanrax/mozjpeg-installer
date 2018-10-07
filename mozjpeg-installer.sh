c34ec3e38257eddf8914610b4e126d25804d8b8a
Andros Fenollosa <andros@fenollosa.email>
2018-10-08 01:09:27 +0200

    First commit

---
+++ mozjpeg-installer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+# Tools install
+sudo apt-get install -y autoconf automake libtool nasm make pkg-config git
+
+# Get code
+git clone https://github.com/mozilla/mozjpeg.git --depth 1
+
+# Build
+cd mozjpeg
+autoreconf -fiv
+./configure
+make -j8
+
+# Install
+sudo make install
+
+# Path
+sudo ln -s /opt/mozjpeg/bin/jpegtran /usr/bin/mozjpeg
+
+# Remove old files
+cd ..
+sudo rm -r mozjpeg
+
+# Alert
+echo 'Finish! Try "mozjpeg --help"'
